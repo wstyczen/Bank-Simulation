c312036586832974428ecf2bec63776552bac9ff
Patryk Adrian Zdziech <01159035@pw.edu.pl>
2021-05-20 12:21:23 +0000

    Upload New File

---
+++ users_input.h
@@ -0,0 +1,11 @@
+#ifndef USERS_INPUT_H
+#define USERS_INPUT_H
+#include <iostream>
+using std::string;
+
+void wrongInput();
+int getNumberFromUser();
+bool checkWord(string str);
+string getStringFromUser();
+
+#endif
