b6813fb62e3689d045954dcf93818fe90c6bebce
Patryk Adrian Zdziech <01159035@pw.edu.pl>
2021-05-20 13:58:52 +0000

    Update permision.h

--- permision.h
+++ permision.h
@@ -2,15 +2,15 @@
 #define PERMISIONS_H
 #include <iostream>
 #include <map>
-#include <bits/stdc++.h>
-using std::map; using std::unordered_set; using std::string;
+#include <set>
+using std::map; using std::set; using std::string;
 
 enum permision{analitic,bank_assistant,cashier,director,financial_manager,data_specialist,account_manager};
 enum responsibility {cash_withdraw,transfer,fast_transfer,deposit_cash,retrieve_user_data,change_user_data,check_account,present_credit_offer,present_account_offer,new_user_help};
 
 string EnumToStr(permision p);
 string EnumToStr(responsibility p);
-extern map<permision, unordered_set <responsibility>> possibleResponsibilities;
+extern map<permision, set<responsibility>> possibleResponsibilities;
 
 permision StrToPermision(string p);
 responsibility StrToResponsibility(string p);
