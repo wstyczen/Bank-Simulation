c2af4782599a8d0eacd7acb65b5c51e91c640399
Patryk Adrian Zdziech <01159035@pw.edu.pl>
2021-05-20 12:19:11 +0000

    Upload New File

---
+++ Worker.h
@@ -0,0 +1,42 @@
+#ifndef WORKER_H_
+#define WORKER_H_
+
+#include "Human.h"
+#include <bits/stdc++.h>
+#include "permision.h"
+#include <set>
+
+namespace std {
+
+class Worker: public Human {
+
+static set<int> availableNumbers;
+
+int employeeNumber,stationNumber;
+set <permision> permissions;
+set <responsibility> responsibilities;
+
+public:
+	Worker(string name, string surname);
+	virtual ~Worker();
+	int getEmployeeNumber() const;
+	void setEmployeeNumber(int empoyeeNumber);
+	void setEmployeeNumber();
+	int getStationNumber() const;
+	void setStationNumber(int stationNumber);
+	const set<permision>& getPermisionsGranted() const;
+	void addPermision(permision permisionGranted);
+	void removePermision(permision permisionToRemove);
+	void clearPermisions();
+	bool canHaveResposibility(const responsibility resp);
+	bool addResponsibility(responsibility responsibilityAdded);
+	void removeResposibility(responsibility responsibilityToRemove);
+	void clearResponsiblities();
+	const set<responsibility>& getResponsibilities() const;
+};
+bool operator<(const Worker& l, const Worker& r);
+ostream& operator<<(ostream& os, const Worker &worker);
+}
+#endif /* WORKER_H_ */
+
+//	Worker(const Worker &other);
